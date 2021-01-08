cd92a00e46b15646e1f61b1ab00af4287b658f42
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-08 10:15:56 -0700

    Access poem api via SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,11 @@
 class SearchController < ApplicationController
   def index
-    require "pry"
-    binding.pry
+    author = params[:author]
+
+    conn = Faraday.new(url: "https://poetrydb.org/")
+
+    response = conn.get("author,poemcount/Emily;10")
+
+    @poems = JSON.parse(response.body, symbolize_names: true)
   end
 end
